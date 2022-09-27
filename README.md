979d744a0a4ffc681f834097fe8ed9c4e51af900
yamaccu <yus1206@hotmail.com>
2022-09-28 01:19:03 +0900

    first

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Mineclaft-SchematicToMcstructure
-JAVA版のストラクチャーデータ（.Schematic）を、BE版のストラクチャーデータ（.mcstructure）に変換するプログラム
+マインクラフト JAVA版のストラクチャーデータ（.Schematic）を、BE版のストラクチャーデータ（.mcstructure）に変換します。  
+
+## 必要ライブラリ
+[Amulet-Core](https://pypi.org/project/amulet-core/)  
+[wxpython](https://pypi.org/project/wxPython/)
+
+## 使い方
+1. SchematicToMcstructure.pyを実行
+2. ファイル選択画面が開くので、.schematicファイルを選択
+3. 同じフォルダに、<同じ名前.mcstructure>が出力される
---
+++ SchematicToMcstructure.py
@@ -0,0 +1,45 @@
+import amulet
+from amulet.level.formats.mcstructure import MCStructureFormatWrapper
+import wx
+import logging
+import sys
+
+log = logging.getLogger(__name__)
+
+app = wx.App()
+with wx.FileDialog(
+    None,
+    'Select Schematic File',
+    wildcard="|".join(
+        [
+            "Legacy Schematic file (*.schematic)|*.schematic",
+            "All files (*.construction;*.mcstructure;*.schematic)|*.construction;*.mcstructure;*.schematic",
+        ]),
+    style=wx.FD_OPEN) as dialog:
+    if dialog.ShowModal() == wx.ID_OK:
+        importpath = dialog.GetPaths()[0]
+    else:
+        sys.exit()
+
+level = amulet.load_level(importpath)
+selection =  level.bounds(level.dimensions[0])
+#log.info(selection)
+
+index1 = importpath.rfind("\\")
+index2 = importpath.rfind(".")
+filename = importpath[index1+1:index2]
+exportpath = importpath[0:index1] + "\\" + filename + ".mcstructure"
+#log.info(index1)
+#log.info(index2)
+#log.info(exportpath)
+#log.info(filename)
+wrapper = MCStructureFormatWrapper(exportpath)
+wrapper.create_and_open("bedrock", [1, 19, 0], selection, False)
+wrapper_dimension = wrapper.dimensions[0]
+#log.info(chunk_count)
+#log.info(selection.chunk_locations())
+for (cx, cz) in selection.chunk_locations():
+    chunk = level.get_chunk(cx, cz, level.dimensions[0])
+    wrapper.commit_chunk(chunk, wrapper_dimension)
+wrapper.save()
+wrapper.close()
